26c478732eb301fdd27455d9adb396899c22701f
shtyagi22 <sh.tyagi22@gmail.com>
2022-10-10 21:12:35 -0400

    imported interviews data and appointments data into the app using axios api get calls, and display the appointments on the app.

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,49 +1,10 @@
 //import React from "react";
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment/index.js";
 import axios from "axios";
-
-
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
+import { getAppointmentsForDay, getInterview } from "../helpers/selectors";
 
 
 export default function Application(props) {
@@ -51,26 +12,39 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    // you may put the line below, but will have to remove/comment hardcoded appointments variable
-    //appointments: {}
+    //you may put the line below, but will have to remove/comment hardcoded appointments variable
+    appointments: {},
+    interviewers: {}
   });
 
-  const appointmentList = Object.values(appointments).map((appointment) => {
+  const setDay = day => setState({ ...state, day });
+  //const setDays = (days) => setState({ ...state, days })
+
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+
+  const appointmentList = dailyAppointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+
     return (
       <Appointment
         key={appointment.id}
         id={appointment.id}
         time={appointment.time}
-        interview={appointment.interview}
+        interview={interview}
       />
 
     )
   });
   useEffect(() => {
-    axios.get("/api/days").then((response) => {
-      console.log(response);
-      setDays(response.data)
-    })
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
+    ])
+      .then((all) => {
+        console.log(all);
+        setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+      })
   }, [])
 
 
@@ -85,7 +59,7 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={setState.days} value={setState.day} onChange={.....}
+            days={state.days} value={state.day} onChange={setDay}
           />
         </nav>
         <img
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,17 +1,25 @@
-export function getAppointmentsForDay(state, day) {
+ export function getAppointmentsForDay(state, day) {
 
   const dayObj = state.days.find(weekday => weekday.name === day);
   if (!dayObj) {
     return [];
   }
-
   const appointments = [];
-
   for (const id of dayObj.appointments) {
     const appointment = state.appointments[id];
-   
-      appointments.push(appointment)
-    
+    appointments.push(appointment)
   }
   return appointments;
-}
+};
+
+export function getInterview(state, interview){
+  if(!interview){
+    return null;
+  }
+const interviewObj = {};
+interviewObj.interviewer = state.interviewers[interview.interviewer];
+interviewObj.student = interview.student;
+console.log("interviewObj:",interviewObj)
+return interviewObj;
+};
+
